2b5ae6a56380bd4a2d9024e773b0073e5f343cc7
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-07 19:30:07 +0530

    Define MongoDB User schema with fields for user data and password hashing. Implement email validation and password comparison methods.

---
+++ models/user-model.ts
@@ -0,0 +1,95 @@
+require("dotenv").config();
+
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const emailRegexPattern: RegExp = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  comparePassword: (enteredPassword: string) => Promise<boolean>;
+}
+
+const userSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+      maxLength: [30, "Your name cannot exceed 30 characters"],
+      minLength: [4, "Your name should have at least 4 characters"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+
+      validate: {
+        validator: function (email: string) {
+          return emailRegexPattern.test(email);
+        },
+        message: "Please enter a valid email",
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minLength: [6, "Your password should have at least 6 characters"],
+      select: false,
+    },
+
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+//  Hashing password before saving user to database
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+//  Compare user password
+userSchema.methods.comparePassword = async function (
+  enteredPassword: string,
+): Promise<boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const UserModel: Model<IUser> = mongoose.model("User", userSchema);
+
+export default UserModel;
