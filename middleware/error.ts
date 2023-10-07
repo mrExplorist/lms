a4292d0c3a3bfd97a7a97a41de6ec6d74337eeb2
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-07 19:28:59 +0530

    Implement ErrorMiddleware for Express to handle various error types, set appropriate status codes and error messages, and send structured error responses.

---
+++ middleware/error.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from "express";
+import ErrorHandler from "../utils/errorHandler";
+
+export const ErrorMiddleWare = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  // Wrong Mongodb ID error
+  if (err.name === "CastError") {
+    const message = `Resource not found. Invalid: ${err.path}`;
+    err = new ErrorHandler(400, message);
+  }
+
+  // Duplication key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(400, message);
+  }
+
+  // wrong jwt error
+  if (err.name === "JsonWebTokenError") {
+    const message = "JSON Web Token is invalid. Try Again!!!";
+    err = new ErrorHandler(400, message);
+  }
+
+  // JWT Expired Error
+  if (err.name === "TokenExpiredError") {
+    const message = "JSON Web Token is expired. Try Again!!!";
+    err = new ErrorHandler(400, message);
+  }
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+};
