12d4e27ddc1e1457331b68c72c6ff039b761508c
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-07 19:22:44 +0530

    Add Redis connection configuration for caching with error handling for missing configuration.

---
+++ utils/redis.ts
@@ -0,0 +1,13 @@
+import { Redis } from "ioredis";
+
+require("dotenv").config();
+
+const redisClient = () => {
+  if (process.env.REDIS_URL) {
+    console.log("Redis url is connected and is", process.env.REDIS_URL);
+    return process.env.REDIS_URL;
+  }
+  throw new Error("Redis url is not defined");
+};
+
+export const redis = new Redis(redisClient());
