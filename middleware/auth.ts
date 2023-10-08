a997b883314fb44f5156b5bec4fc0e6946837725
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-08 15:25:59 +0530

    Implement middleware to check if a user is authenticated by verifying the access token, retrieving user data from Redis, and attaching it to the request.

---
+++ middleware/auth.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import { CatchAsyncError } from "./catchAsyncErrors";
+import ErrorHandler from "../utils/errorHandler";
+import jwt from "jsonwebtoken";
+import { redis } from "../utils/redis";
+
+// Authenticated user
+export const isAuthenticatedUser = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { accessToken } = req.cookies;
+
+    if (!accessToken) {
+      return next(
+        new ErrorHandler(401, "Please login to access this resource"),
+      );
+    }
+
+    const decoded = jwt.verify(
+      accessToken,
+      process.env.ACCESS_TOKEN_SECRET as string,
+    );
+
+    if (!decoded) {
+      return next(
+        new ErrorHandler(401, "Access token is invalid or has expired"),
+      );
+    }
+
+    const user = await redis.get(`session:${(decoded as any).id}`);
+
+    if (!user) {
+      return next(
+        new ErrorHandler(401, "User Not Found, Please login to access"),
+      );
+    }
+
+    req.user = JSON.parse(user);
+
+    next();
+  },
+);
