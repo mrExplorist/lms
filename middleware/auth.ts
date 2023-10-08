581439756fc409fbf7ef69639663b4ad3890a931
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-08 15:52:12 +0530

    Implemented role-based authorization using the authorizeRoles middleware to control access to specific routes based on user roles.

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -39,3 +39,18 @@ export const isAuthenticatedUser = CatchAsyncError(
     next();
   },
 );
+
+//  validate user role
+export const authorizeRoles = (...roles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user?.role || "")) {
+      return next(
+        new ErrorHandler(
+          403,
+          `Role (${req.user?.role}) is not allowed to access this resource`,
+        ),
+      );
+    }
+    next();
+  };
+};
