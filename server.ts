6fd56d7c719f4291b8fb4dd9d47b8755b95cc3dc
Lalit sharma <geekymonkcp742k@gmail.com>
2023-10-09 00:42:45 +0530

    Updated server initialization to include Cloudinary configuration.

--- server.ts
+++ server.ts
@@ -1,9 +1,16 @@
 import { app } from "./app";
 import connectDB from "./utils/db";
+import { v2 as cloudinary } from "cloudinary";
 require("dotenv").config();
 
-// create server
+// cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
 
+// create server
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);
   connectDB();
